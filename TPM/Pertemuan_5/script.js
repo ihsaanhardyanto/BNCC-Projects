b8321e60ded87968081e3a41135c148379affa41
u-rated <ihsaan.hardyanto@gmail.com>
2023-07-31 17:42:28 +0700

    first-commit

---
+++ TPM/Pertemuan_5/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#box2').hide()
+    $('#box').click(function(){
+        $('#box').hide(1000, function(){
+            })
+        })
+    })
+
+    
+
---
+++ TPM/Ref - Pertemuan 4/js/script.js
@@ -0,0 +1,7 @@
+function _id(id){
+    return document.getElementById(id);
+}
+
+function _class(className){
+    return document.getElementsByClassName(className);
+}
